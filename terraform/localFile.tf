ab0ef6f67cc23109de07776c8112c37835747785
Sumanjeet <sumanjeet0012@gmail.com>
2025-01-15 16:56:35 +0530

    feat: add random pet name generator with terraform

--- terraform/localFile.tf
+++ terraform/localFile.tf
@@ -2,3 +2,9 @@ resource "local_file" "pet" {
     filename = "test/animals.txt"
     content = "I Love Cats"
 }
+
+resource "random_pet" "name" {
+    prefix = "Mrs"
+    separator = "."
+    length = 1
+}
